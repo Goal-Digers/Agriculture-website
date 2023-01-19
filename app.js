94ef42120ef28be88aade74269fe73fc41bed805
Kumudha <79578963+Kumudhaart@users.noreply.github.com>
2023-01-19 14:51:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const app=express();
+app.use(express.static(__dirname+"/public"));
+app.get("/",function(req,res){
+   res.sendFile(__dirname+"/Homepage.html");
+});
+app.get("/signup.html",function(req,res){
+res.sendFile(__dirname+"/signup.html")
+});
+app.get("/farmer.html",function(req,res){
+    res.sendFile(__dirname+"/farmer.html");
+});
+app.get("/customer.html",function(req,res){
+    res.sendFile(__dirname+"/customer.html");
+});
+app.get("/business.html",function(req,res){
+    res.sendFile(__dirname+"/business.html");
+});
+app.get("/student.html",function(req,res){
+    res.sendFile(__dirname+"/student.html");
+});
+app.listen(3000,function(){
+    console.log("hey my server is running");
+});
